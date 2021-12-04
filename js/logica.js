bdcfd85415af67d478e15370eb48c03c6bb69c89
Kar077 <kontrerazqqarla@gmail.com>
2021-12-03 21:23:05 -0600

    version 1

---
+++ js/logica.js
@@ -0,0 +1,14 @@
+function calculartabla(){
+	var cadena="";
+	var num=parseInt(document.getElementById("numm").value);
+
+for(var i=0;i<=10;i++){
+cadena=cadena+"\n"+num+" X "+i+" = "+(num*i);
+
+}
+function ACCEDE(){
+	<p><a href="https://www.w3schools.com/">Visit W3Schools.com!</a></p>
+}
+
+document.getElementById("restabla").value=cadena;
+}
